915c610184df76d7c1998214da21aa832bd9dd47
sergey <sergio_89@ukr.net>
2018-09-22 10:54:47 +0300

    raw query example for todos added

--- src/main/groovy/com/graphql/demo/client/Config.java
+++ src/main/groovy/com/graphql/demo/client/Config.java
@@ -2,5 +2,5 @@ package com.graphql.demo.client;
 
 public class Config {
 
-    public static final String URL = "http://localhost:40491";
+    public static final String URL = "http://localhost:34631";
 }
--- src/test/groovy/todo/ToDoGraphQLTest.groovy
+++ src/test/groovy/todo/ToDoGraphQLTest.groovy
@@ -2,14 +2,37 @@ package todo
 
 import com.graphql.demo.client.Argument
 import com.graphql.demo.client.GraphqlClient
+import com.graphql.demo.client.GraphqlResponse
 import com.graphql.demo.client.GrapqlArguments
 import com.graphql.demo.client.QueryGenerator
+import org.hamcrest.Matchers
 import spock.lang.Specification
 import todo.model.Response
+
+import static org.hamcrest.Matchers.hasSize
 
 class ToDoGraphQLTest extends Specification{
 
     private GraphqlClient graphqlClient = new GraphqlClient("https://graffiti-todo.herokuapp.com/graphql")
+
+    def "test can get all todos using raw query"(){
+        given:
+
+        def query = """
+              query { 
+                  todos (complete:false) { 
+                    text 
+                    complete 
+                  } 
+              }
+        """
+        when:
+        def response = graphqlClient.executeQuery(query)
+
+        then:
+        response.assertThat("data.todos", hasSize(3))
+    }
+
 
     def "test can get all todos"() {
         given:
@@ -19,13 +42,13 @@ class ToDoGraphQLTest extends Specification{
         def resp = graphqlClient.executeQuery(query).asPojo(GetToDosResponse)
 
         then:
-        resp.data.todos.size() == 7
+        resp.data.todos.size() == 3
 
     }
 }
 
 @GrapqlArguments(
-    @Argument(name = "complete", val = "true")
+    @Argument(name = "complete", val = "false")
 )
 class Todos {
     String text
